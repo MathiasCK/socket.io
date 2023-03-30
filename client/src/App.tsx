4524aaf3656093f7c0449c5a9eccb87daf86a7c1
mathiasck <mathias.ck2000@gmail.com>
2023-03-30 13:55:25 +0200

    client: setup events & rooms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import * as io from "socket.io-client";
 
-function App() {
+const App = () => {
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messagesReceived, setMessagesReceived] = useState([]);
+
+  const socket = io.connect("http://localhost:8080");
+
+  useEffect(() => {
+    socket.on("receive_message", data => {
+      // @ts-ignore
+      setMessagesReceived([...messagesReceived, data]);
+    });
+  }, [socket, messagesReceived]);
+
+  const sendMessage = () => {
+    if (message !== "") {
+      socket.emit("send_message", {
+        id: socket.id,
+        message,
+        room,
+      });
+    }
+  };
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div>
+      <input
+        type="text"
+        placeholder="Room number ..."
+        onChange={e => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join room</button>
+      <input
+        onChange={e => {
+          setMessage(e.target.value);
+        }}
+        type="text"
+        placeholder="Message ..."
+      />
+      <button onClick={sendMessage}>Send message</button>
+      {messagesReceived.map((data: any) => (
+        <p key={data.id}>
+          {data.id} : {data.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
